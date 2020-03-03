666ea7c5b0ab22c6de8cf3fae7d854ae6253d388
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-03 01:28:09 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,12 +5,12 @@ require 'directors_database'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   nil
-  names=["Jaws", "E.T","Schindler"]
+  name=["Jaws", "E.T","Schindler"]
   movies= ["Close Encounters of the Third Kind","Close Encounters of the Third Kind", "the Extra-terrestrial list "  ]
   a_o_a= [[1,2,3],[456],[7,8,9]]
 end
 
-  pp names
+  pp name
   pp movies
   pp a_o_a
 
